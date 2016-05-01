41239cba46740086f4bb28df97695dcde311192b
Oanh Vu <vuoanh@cs.grinnell.edu>
2016-05-01 15:04:21 -0500

    changed .hh files and main file, working on Makefile

--- bitmap.hh
+++ bitmap.hh
@@ -9,16 +9,20 @@
 #include <cstring>
 
 #include "vec.hh"
-
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#else
+#define CUDA_CALLABLE_MEMBER
+#endif
 class bitmap {
 public:
   // Constructor: set up the bitmap width, height, and data array
-  bitmap(size_t width, size_t height) : _width(width), _height(height) {
+  CUDA_CALLABLE_MEMBER bitmap(size_t width, size_t height) : _width(width), _height(height) {
     _data = new rgb32[width*height];
   }
   
   // Destructor: free the data array
-  ~bitmap() {
+ CUDA_CALLABLE_MEMBER ~bitmap() {
     delete _data;
   }
   
@@ -26,7 +30,7 @@ public:
   size_t size() { return _width*_height*sizeof(rgb32); }
   
   // Copy this bitmap to a given data location
-  void copy_to(void* dest) {
+ CUDA_CALLABLE_MEMBER void copy_to(void* dest) {
     memcpy(dest, _data, size());
   }
   
@@ -39,13 +43,13 @@ public:
   bitmap& operator=(bitmap&&) = delete;
   
   // Get the height of the bitmap
-  size_t height() { return _height; }
+  CUDA_CALLABLE_MEMBER size_t height() { return _height; }
   
   // Get the width of the bitmap
-  size_t width() { return _width; }
+  CUDA_CALLABLE_MEMBER size_t width() { return _width; }
   
   // Set the color at a given location
-  void set(size_t x, size_t y, vec c) {
+  CUDA_CALLABLE_MEMBER void set(size_t x, size_t y, vec c) {
     assert(x < _width && "X coordinate is out of bounds");
     assert(y < _height && "Y coordinate is out of bounds");
     _data[y*_width+x] = { 0, (uint8_t)fmax(0, fmin(255, 255*c.x())),
--- vec.hh
+++ vec.hh
@@ -4,26 +4,32 @@
 #include <cmath>
 
 #include "bitmap.hh"
+//http://stackoverflow.com/questions/6978643/cuda-and-classes
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#else
+#define CUDA_CALLABLE_MEMBER
+#endif
 
 class vec {
 public:
   // Create a vector
-  vec(float x, float y, float z) : _x(x), _y(y), _z(z) {}
+  CUDA_CALLABLE_MEMBER vec(float x, float y, float z) : _x(x), _y(y), _z(z) {}
   
-  vec() : _x(0), _y(0), _z(0) {}
+  CUDA_CALLABLE_MEMBER vec() : _x(0), _y(0), _z(0) {}
   
   // Getters for x, y, and z
-  float x() { return _x; }
-  float y() { return _y; }
-  float z() { return _z; }
+  CUDA_CALLABLE_MEMBER float x() { return _x; }
+  CUDA_CALLABLE_MEMBER float y() { return _y; }
+  CUDA_CALLABLE_MEMBER float z() { return _z; }
   
   // Add another vector to this one and return the result
-  vec operator+(const vec& other) {
+  CUDA_CALLABLE_MEMBER vec operator+(const vec& other) {
     return vec(_x + other._x, _y + other._y, _z + other._z);
   }
   
   // Add another vector to this one and update in place
-  vec& operator+=(const vec& other) {
+  CUDA_CALLABLE_MEMBER vec& operator+=(const vec& other) {
     _x += other._x;
     _y += other._y;
     _z += other._z;
@@ -31,17 +37,17 @@ public:
   }
   
   // Negate this vector
-  vec operator-() {
+  CUDA_CALLABLE_MEMBER vec operator-() {
     return vec(-_x, -_y, -_z);
   }
   
   // Subtract another vector from this one and return the result
-  vec operator-(const vec& other) {
+  CUDA_CALLABLE_MEMBER vec operator-(const vec& other) {
     return vec(_x-other._x, _y-other._y, _z-other._z);
   }
   
   // Subtract another vector from this one and update in place
-  vec& operator-=(const vec& other) {
+  CUDA_CALLABLE_MEMBER vec& operator-=(const vec& other) {
     _x -= other._x;
     _y -= other._y;
     _z -= other._z;
@@ -49,12 +55,12 @@ public:
   }
   
   // Multiply this vector by a scalar and return the result
-  vec operator*(float scalar) {
+  CUDA_CALLABLE_MEMBER vec operator*(float scalar) {
     return vec(_x*scalar, _y*scalar, _z*scalar);
   }
   
   // Multiply this vector by a scalar and update in place
-  vec& operator*=(float scalar) {
+  CUDA_CALLABLE_MEMBER vec& operator*=(float scalar) {
     _x *= scalar;
     _y *= scalar;
     _z *= scalar;
@@ -62,12 +68,12 @@ public:
   }
   
   // Divide this vector by a scalar and return the result
-  vec operator/(float scalar) {
+  CUDA_CALLABLE_MEMBER vec operator/(float scalar) {
     return vec(_x/scalar, _y/scalar, _z/scalar);
   }
   
   // Divide this vector by a scalar and update in place
-  vec& operator/=(float scalar) {
+  CUDA_CALLABLE_MEMBER vec& operator/=(float scalar) {
     _x /= scalar;
     _y /= scalar;
     _z /= scalar;
@@ -75,48 +81,48 @@ public:
   }
   
   // Compute the dot product of this vector with another vector
-  float dot(const vec& other) {
+  CUDA_CALLABLE_MEMBER float dot(const vec& other) {
     return _x*other._x + _y*other._y + _z*other._z;
   }
   
   // Compute the cross product of this vector with another vector
-  vec cross(const vec& other) {
+  CUDA_CALLABLE_MEMBER vec cross(const vec& other) {
     return vec(_y * other._z - _z * other._y,
                _z * other._x - _x * other._z,
                _x * other._y - _y * other._x);
   }
   
   // Compute the hadamard, or component-wise product with another vector
-  vec hadamard(const vec& other) {
+  CUDA_CALLABLE_MEMBER vec hadamard(const vec& other) {
     return vec(_x * other._x, _y * other._y, _z * other._z);
   }
   
   // Compute the magnitude of this vector
-  float magnitude() {
+ CUDA_CALLABLE_MEMBER  float magnitude() {
     return sqrt(pow(_x, 2) + pow(_y, 2) + pow(_z, 2));
   }
   
   // Compute a normalized version of this vector
-  vec normalized() {
+  CUDA_CALLABLE_MEMBER vec normalized() {
     return (*this) / this->magnitude();
   }
   
   // Compute a new vector rotated around the x axis
-  vec xrotated(float radians) {
+  CUDA_CALLABLE_MEMBER vec xrotated(float radians) {
     return vec(_x,
                cos(radians) * _y - sin(radians) * _z,
                sin(radians) * _y + cos(radians) * _z);
   }
   
   // Compute a new vector rotated around the y axis
-  vec yrotated(float radians) {
+  CUDA_CALLABLE_MEMBER vec yrotated(float radians) {
     return vec(cos(radians) * _x + sin(radians) * _z,
                _y,
                -sin(radians) * _x + cos(radians) * _z);
   }
   
   // Compute a new vector rotated around the z axis
-  vec zrotated(float radians) {
+ CUDA_CALLABLE_MEMBER  vec zrotated(float radians) {
     return vec(cos(radians) * _x - sin(radians) * _y,
                sin(radians) * _x + cos(radians) * _y,
                _z);
@@ -124,7 +130,7 @@ public:
   
   // Compute a new vector rotated around another vector
   // Source: https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
-  vec rotated_around(vec& k, float theta) {
+  CUDA_CALLABLE_MEMBER vec rotated_around(vec& k, float theta) {
     vec& v = *this;
     return v * cos(theta) + k.cross(v) * sin(theta) + k * k.dot(v) * (1 - cos(theta));
   }
